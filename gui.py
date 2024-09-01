54d497ef325f5353bb5cc1d0db9e398f8e941f97
anilergan <ergananil@gmail.com>
2024-09-01 14:09:23 +0300

    Reviewed. Push.

--- gui.py
+++ gui.py
@@ -119,7 +119,7 @@ class BlackjackGUI(QMainWindow, BlackjackDynamics):
         self.set_status_board()
         self.set_players_cocktail_and_smoke()
         self.announce()
-        self.payout()é
+        self.payout()
         self.set_budget()
 
 # SİGNAL-SLOTS Functions -------------------------------------
